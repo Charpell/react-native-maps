4c5574f970e1c513d2a11ea3a9d81efd4b0f1352
Ebuka Umeh <charpellumeh@gmail.com>
2022-12-07 02:09:38 +0100

    initial setup

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.02;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const INITIAL_POSITION = {
+  latitude: 40.76711,
+  longitude: -73.979704,
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA,
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView
+       style={styles.map}  
+       provider={PROVIDER_GOOGLE} 
+       initialRegion={INITIAL_POSITION}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
